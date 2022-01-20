a388c2ea087dab0fd392a71cf5469e87e9d2123f
Suelen Arruda <suelenonyrius@gmail.com>
2022-01-20 17:25:46 +0100

    add informaçoes no botao do carrinho

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,13 +1,13 @@
-const fetchItem = async (ItemID) => {
+const fetchItem = async (id) => { 
   try {
- const url = `https://api.mercadolibre.com/items/${ItemID}`;
- const response = await fetch(url);
- const data = await response.json();
- return data;
+   const url = `https://api.mercadolibre.com/items/${id}`; 
+   const response = await fetch(url);
+   const data = await response.json();
+   return data;
   } catch (error) {
-    return error;
+   return error;
   }
-};
+ };
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- script.js
+++ script.js
@@ -12,29 +12,51 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ sku, name, image, salePrice }) {
   const section = document.createElement('section');
+  const price = salePrice.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
   section.className = 'item';
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createCustomElement('span', 'item__price', price));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
   return section;  
 }
-/* function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-} */
+/** Source: adaptar para moeda brasileira 
+ * https://www.horadecodar.com.br/2020/09/01/formatar-moeda-brasileira-em-javascript-float-para-real/
+ */
 
-/* function cartItemClickListener() {
-  // coloque seu código aqui
-}
+// pega os ids dos itens para serem colocados no carrinho
+ function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+} 
+/** Source: sobre parent element
+ * https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_node_parentelement
+ */
+ function cartItemClickListener() {
+   
+  }
+
 function createCartItemElement({ sku, name, salePrice }) {
     const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-} */
+}
+
+const addCartProductItemID = async ({ target }) => {
+  const getId = getSkuFromProductItem(target.parentElement); 
+  const itemsProducts = await fetchItem(getId);
+  console.log(itemsProducts);
+}
+const addClickOnItemProductBtn = () => {
+  const allProductsButtons = document.querySelectorAll('.item__add');
+  allProductsButtons.forEach((button) => {
+    button.addEventListener('click', addCartProductItemID);
+  });
+};
 
 // trazendo as funçoes assincronas
 async function loadProducts() {
@@ -51,12 +73,12 @@ async function loadProducts() {
    sectionItem.appendChild(createProductItemElement(objectProduct));
  });
 }
-async function loadItemsCart() {
-  const itemProduct = await fetchItem(IdProduct); 
-  return itemProduct;
-}
-
-window.onload = () => { 
-  loadProducts();
-  loadItemsCart();
+ 
+window.onload = async () => { 
+ await loadProducts();
+ addClickOnItemProductBtn();
 };
+/** Source: 
+ * projeto densenvolvido com o auxilio do repositorio do Thiago Nobrega:
+ * https://github.com/tryber/sd-018-b-project-shopping-cart/pull/30/files
+ */
